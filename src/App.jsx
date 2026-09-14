f6f053b69de6929782b71387304a919a44f8ff5d
rscode999 <rscode999@outlook.com>
2026-09-13 22:18:42 -0400

    fix no render error on homepage load

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,24 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
-import Home from './pages/Home.jsx'
-import Projects from './pages/Projects.jsx'
-import Layout from './components/Layout.jsx'
-
-
-function ScrollToTop() {
-  const { pathname } = useLocation();
-  
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-
-  return null;
-}
+import Home from './pages/Home.jsx';
+import Projects from './pages/Projects.jsx';
+import Layout from './components/Layout.jsx';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <ScrollToTop/>
+    <HashRouter>
+      <Layout>
         <Routes>
-          <Route>
-            <Route path="/" element={<Home />} />
-            <Route path="*" element={<Home />} />
-            <Route path="/projects" element={<Projects />} />
-          </Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="*" element={<Home />} />
         </Routes>
-    </>
+      </Layout>
+    </HashRouter>
   );
 }
 
